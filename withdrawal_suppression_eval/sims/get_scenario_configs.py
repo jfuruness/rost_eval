db0735dc9ee2a3b048c9d37fb057493326d39d15
Justin Furuness <jfuruness@gmail.com>
2024-12-12 18:49:37 -0500

    ruff format [skip ci]

--- withdrawal_suppression_eval/sims/get_scenario_configs.py
+++ withdrawal_suppression_eval/sims/get_scenario_configs.py
@@ -10,39 +10,34 @@ from withdrawal_suppression_eval.scenarios import VictimsPrefixWithdrawalScenari
 
 
 def get_scenario_configs():
-
     bgp_dag = CAIDAASGraphConstructor(tsv_path=None).run()
     asns = tuple(bgp_dag.as_dict.values())
 
     def get_percentage_hardcoded_asn_cls_dict(percent):
         k = percent * len(bgp_dag)
-        return frozendict(
-            {
-                asn: DropWithdrawalsFull for asn in random.sample(asns, k)
-            }
-        )
+        return frozendict({asn: DropWithdrawalsFull for asn in random.sample(asns, k)})
 
-    five_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(.05)
-    ten_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(.1)
-    twenty_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(.2)
+    five_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.05)
+    ten_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.1)
+    twenty_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.2)
 
     return (
         ScenarioConfig(
             BasePolicyCls=BGPFull,
             ScenarioCls=VictimsPrefixWithdrawalScenario,
             hardcoded_asn_cls_dict=five_percent_hardcoded_asn_cls_dict,
-            scenario_label="5% Dropping Withdrawals"
+            scenario_label="5% Dropping Withdrawals",
         ),
         ScenarioConfig(
             BasePolicyCls=BGPFull,
             ScenarioCls=VictimsPrefixWithdrawalScenario,
             hardcoded_asn_cls_dict=ten_percent_hardcoded_asn_cls_dict,
-            scenario_label="10% Dropping Withdrawals"
+            scenario_label="10% Dropping Withdrawals",
         ),
         ScenarioConfig(
             BasePolicyCls=BGPFull,
             ScenarioCls=VictimsPrefixWithdrawalScenario,
             hardcoded_asn_cls_dict=twenty_percent_hardcoded_asn_cls_dict,
-            scenario_label="20% Dropping Withdrawals"
+            scenario_label="20% Dropping Withdrawals",
         ),
     )
