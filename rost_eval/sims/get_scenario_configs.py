5e2829b13b4c746fc8a7acc6de805cea684c96c8
Justin Furuness <jfuruness@gmail.com>
2024-12-24 03:48:00 -0500

    100 trials complete, seems to be working

--- rost_eval/sims/get_scenario_configs.py
+++ rost_eval/sims/get_scenario_configs.py
@@ -3,10 +3,10 @@ import random
 from frozendict import frozendict
 
 from bgpy.as_graphs import CAIDAASGraphConstructor
+from bgpy.shared.enums import ASGroups
 from bgpy.simulation_engine import (
     BGPFullIgnoreInvalid,
     BGPFullSuppressWithdrawals,
-    BGPFull,
     RoSTFull,
 )
 from bgpy.simulation_framework import ScenarioConfig
@@ -16,6 +16,16 @@ from rost_eval.scenarios import VictimsPrefixWithdrawalScenario
 def get_scenario_configs():
     bgp_dag = CAIDAASGraphConstructor(tsv_path=None).run()
     asns = tuple(bgp_dag.as_dict)
+
+    one_t1_hardcoded_asn_cls_dict = frozendict(
+        {  # type: ignore
+            asn: BGPFullSuppressWithdrawals
+            for asn in random.sample(
+                bgp_dag.asn_groups[ASGroups.INPUT_CLIQUE.value],
+                1,  # type: ignore
+            )
+        }
+    )
 
     def get_percentage_hardcoded_asn_cls_dict(percent):
         k = int(percent * len(asns))
@@ -28,20 +38,27 @@ def get_scenario_configs():
     )
     one_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.01)
     five_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.05)
-    twenty_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.2)
-    hundred_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(1)
+    # twenty_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.2)
+    # hundred_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(1)
 
     return (
-        ScenarioConfig(
-            BasePolicyCls=BGPFull,
-            ScenarioCls=VictimsPrefixWithdrawalScenario,
-            scenario_label="0% Dropping Withdrawals (No RoST)",
-        ),
+        # ScenarioConfig(
+        #     BasePolicyCls=BGPFull,
+        #     ScenarioCls=VictimsPrefixWithdrawalScenario,
+        #     scenario_label="0% Dropping Withdrawals (No RoST)",
+        # ),
+        # ScenarioConfig(
+        #     BasePolicyCls=BGPFullIgnoreInvalid,
+        #     AdoptPolicyCls=RoSTFull,
+        #     ScenarioCls=VictimsPrefixWithdrawalScenario,
+        #     scenario_label="0% Dropping Withdrawals",
+        # ),
         ScenarioConfig(
             BasePolicyCls=BGPFullIgnoreInvalid,
             AdoptPolicyCls=RoSTFull,
             ScenarioCls=VictimsPrefixWithdrawalScenario,
-            scenario_label="0% Dropping Withdrawals",
+            hardcoded_asn_cls_dict=one_t1_hardcoded_asn_cls_dict,
+            scenario_label="1 Tier-1 AS",
         ),
         ScenarioConfig(
             BasePolicyCls=BGPFullIgnoreInvalid,
@@ -64,17 +81,17 @@ def get_scenario_configs():
             hardcoded_asn_cls_dict=five_percent_hardcoded_asn_cls_dict,
             scenario_label="5% Dropping Withdrawals",
         ),
-        ScenarioConfig(
-            BasePolicyCls=BGPFullIgnoreInvalid,
-            AdoptPolicyCls=RoSTFull,
-            ScenarioCls=VictimsPrefixWithdrawalScenario,
-            hardcoded_asn_cls_dict=twenty_percent_hardcoded_asn_cls_dict,
-            scenario_label="20% Dropping Withdrawals",
-        ),
-        ScenarioConfig(
-            BasePolicyCls=BGPFullIgnoreInvalid,
-            ScenarioCls=VictimsPrefixWithdrawalScenario,
-            hardcoded_asn_cls_dict=hundred_percent_hardcoded_asn_cls_dict,
-            scenario_label="100% Dropping Withdrawals, no RoST",
-        ),
+        # ScenarioConfig(
+        #     BasePolicyCls=BGPFullIgnoreInvalid,
+        #     AdoptPolicyCls=RoSTFull,
+        #     ScenarioCls=VictimsPrefixWithdrawalScenario,
+        #     hardcoded_asn_cls_dict=twenty_percent_hardcoded_asn_cls_dict,
+        #     scenario_label="20% Dropping Withdrawals",
+        # ),
+        # ScenarioConfig(
+        #     BasePolicyCls=BGPFullIgnoreInvalid,
+        #     ScenarioCls=VictimsPrefixWithdrawalScenario,
+        #     hardcoded_asn_cls_dict=hundred_percent_hardcoded_asn_cls_dict,
+        #     scenario_label="100% Dropping Withdrawals, no RoST",
+        # ),
     )
