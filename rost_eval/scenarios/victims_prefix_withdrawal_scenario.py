355100f01c4126a1a24d4348f43647da958dac6c
Justin Furuness <jfuruness@gmail.com>
2024-12-26 04:52:42 -0500

    Victim is no longer possibly suppressing withdrawals

--- rost_eval/scenarios/victims_prefix_withdrawal_scenario.py
+++ rost_eval/scenarios/victims_prefix_withdrawal_scenario.py
@@ -5,7 +5,8 @@ from bgpy.shared.enums import Prefixes, SpecialPercentAdoptions
 from bgpy.simulation_framework import ValidPrefix
 
 if TYPE_CHECKING:
-    from bgpy.simulation_engine import BaseSimulationEngine
+    from bgpy.as_graph import AS
+    from bgpy.simulation_engine import BaseSimulationEngine, Policy
 
 
 class VictimsPrefixWithdrawalScenario(ValidPrefix):
@@ -38,3 +39,17 @@ class VictimsPrefixWithdrawalScenario(ValidPrefix):
         """We don't want the victim to adopt here"""
 
         return frozenset()
+
+    def get_policy_cls(self, as_obj: "AS") -> type["Policy"]:
+        """Returns the policy class for a given AS to set"""
+
+        asn = as_obj.asn
+        if asn in self.victim_asns:
+            if asn in self.adopting_asns:
+                return self.scenario_config.AdoptPolicyCls
+            else:
+                return self.scenario_config.hardcoded_base_asn_cls_dict.get(
+                    asn, self.scenario_config.BasePolicyCls
+                )
+        else:
+            return super().get_policy_cls(as_obj)
