b37716e84415fe025a089e4f22f1e56e5fb9702a
Justin Furuness <jfuruness@gmail.com>
2025-01-23 14:43:41 -0500

    t1 graphs [skip ci]

--- rost_eval/sims/get_scenario_configs.py
+++ rost_eval/sims/get_scenario_configs.py
@@ -2,8 +2,8 @@
 
 from frozendict import frozendict
 
-# from bgpy.as_graphs import CAIDAASGraphConstructor
-# from bgpy.shared.enums import ASGroups
+from bgpy.as_graphs import CAIDAASGraphConstructor
+from bgpy.shared.enums import ASGroups
 from bgpy.simulation_engine import (
     BGPFullIgnoreInvalid,
     BGPFullSuppressWithdrawals,
@@ -11,13 +11,32 @@ from bgpy.simulation_engine import (
 )
 from bgpy.simulation_framework import ScenarioConfig
 from rost_eval.scenarios import (
-    # VictimsPrefixWithdrawalOnlyCCScenario,
-    VictimsPrefixWithdrawalScenario,
+    VictimsPrefixWithdrawalOnlyCCScenario,
+    # VictimsPrefixWithdrawalScenario,
 )
 
 
 def get_scenario_configs():
-    # bgp_dag = CAIDAASGraphConstructor(tsv_path=None).run()
+    bgp_dag = CAIDAASGraphConstructor(tsv_path=None).run()
+    tier_1_ases = bgp_dag.as_groups[ASGroups.INPUT_CLIQUE.value]
+    tier_1_ases = sorted(tier_1_ases, key=lambda x: x.customer_cone_size, reverse=True)
+    hardcoded_asn_dicts = [
+        (frozendict({x.asn: BGPFullSuppressWithdrawals}), f"{x.asn} suppressing")
+        for x in tier_1_ases[:5]
+    ]
+
+    return [
+        ScenarioConfig(
+            BasePolicyCls=BGPFullIgnoreInvalid,
+            AdoptPolicyCls=RoSTFull,
+            ScenarioCls=VictimsPrefixWithdrawalOnlyCCScenario,
+            hardcoded_asn_cls_dict=hardcoded_list,
+            scenario_label=label,
+        )
+        for hardcoded_list, label in hardcoded_asn_dicts
+    ]
+
+
     # asns = tuple(bgp_dag.as_dict)
 
     # one_t1_hardcoded_asn_cls_dict = frozendict(
@@ -44,42 +63,29 @@ def get_scenario_configs():
     # twenty_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(0.2)
     # hundred_percent_hardcoded_asn_cls_dict = get_percentage_hardcoded_asn_cls_dict(1)
 
-    def get_asns_hardcoded_asn_cls_dict(asns):
-        return frozendict({asn: BGPFullSuppressWithdrawals for asn in asns})
+    # def get_asns_hardcoded_asn_cls_dict(asns):
+    #     return frozendict({asn: BGPFullSuppressWithdrawals for asn in asns})
 
-    peers_1 = get_asns_hardcoded_asn_cls_dict(
-        [749, 14618, 36935, 12880, 46690, 36937, 17858, 37069, 11003, 3378]
-    )
-    peers_5 = get_asns_hardcoded_asn_cls_dict(
-        [9808, 54574, 6713, 1659, 36925, 4249, 14962, 3561, 2764, 37075]
-    )
-    peers_10 = get_asns_hardcoded_asn_cls_dict(
-        [55836, 7303, 9824, 47377, 2116, 37457, 8167, 36914, 12302, 12455]
-    )
-    peers_25 = get_asns_hardcoded_asn_cls_dict(
-        [132825, 7539, 37168, 13489, 2828, 26599, 13094, 30990, 2381, 45543]
-    )
-    peers_50 = get_asns_hardcoded_asn_cls_dict(
-        [4862, 29063, 135607, 12727, 25593, 42845, 39605, 198335, 207456, 62512]
-    )
-    peers_100 = get_asns_hardcoded_asn_cls_dict(
-        [293, 36874, 2818, 8823, 35000, 31727, 39560, 58291, 61098, 196968]
-    )
-    peers_labels = [f"{x} peers" for x in (1, 5, 10, 25, 50, 100)]
-    hardcoded_peers_list = [peers_1, peers_5, peers_10, peers_25, peers_50, peers_100]
-
-    return [
-        ScenarioConfig(
-            BasePolicyCls=BGPFullIgnoreInvalid,
-            AdoptPolicyCls=RoSTFull,
-            ScenarioCls=VictimsPrefixWithdrawalScenario,
-            hardcoded_asn_cls_dict=hardcoded_peers,
-            scenario_label=label,
-        )
-        for hardcoded_peers, label in zip(
-            hardcoded_peers_list, peers_labels, strict=True
-        )
-    ]
+    # peers_1 = get_asns_hardcoded_asn_cls_dict(
+    #     [749, 14618, 36935, 12880, 46690, 36937, 17858, 37069, 11003, 3378]
+    # )
+    # peers_5 = get_asns_hardcoded_asn_cls_dict(
+    #     [9808, 54574, 6713, 1659, 36925, 4249, 14962, 3561, 2764, 37075]
+    # )
+    # peers_10 = get_asns_hardcoded_asn_cls_dict(
+    #     [55836, 7303, 9824, 47377, 2116, 37457, 8167, 36914, 12302, 12455]
+    # )
+    # peers_25 = get_asns_hardcoded_asn_cls_dict(
+    #     [132825, 7539, 37168, 13489, 2828, 26599, 13094, 30990, 2381, 45543]
+    # )
+    # peers_50 = get_asns_hardcoded_asn_cls_dict(
+    #     [4862, 29063, 135607, 12727, 25593, 42845, 39605, 198335, 207456, 62512]
+    # )
+    # peers_100 = get_asns_hardcoded_asn_cls_dict(
+    #     [293, 36874, 2818, 8823, 35000, 31727, 39560, 58291, 61098, 196968]
+    # )
+    # peers_labels = [f"{x} peers" for x in (1, 5, 10, 25, 50, 100)]
+    # hardcoded_peers_list = [peers_1, peers_5, peers_10, peers_25, peers_50, peers_100]
 
     # return (
     #     # ScenarioConfig(
